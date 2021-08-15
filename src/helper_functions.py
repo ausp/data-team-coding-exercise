b7d01e3fd236686f2932e1260ca83c0641f12830
ausp <38692347+ausp@users.noreply.github.com>
2021-08-15 19:19:19 +0100

    Basic try/catch for file reading (#4)

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,24 +1,40 @@
 import pandas as pd
 
 
-def load_file_from_dataframe(file_location: str, file_format: str):
-    if file_format == "json":
-        return pd.read_json(file_location, lines=True)
-    elif file_format == "csv":
-        return pd.read_csv(file_location, delimiter=",", header=0)
-    else:
-        raise Exception(f"File format {file_format} is not recognised")
+def load_file_from_dataframe(file_location: str, file_format: str, fail_on_error=True):
+    try:
+        if file_format == "json":
+            return pd.read_json(file_location, lines=True)
+        elif file_format == "csv":
+            return pd.read_csv(file_location, delimiter=",", header=0)
+        else:
+            raise Exception(f"File format {file_format} is not recognised")
+    except Exception as e:
+        print(e)
+        if fail_on_error is True:
+            raise e
 
 
 def write_dataframe_to_file(
-    df: pd.DataFrame, file_location: str, file_format: str = "csv", header=True, mode: str = "x", sep=","
+    df: pd.DataFrame,
+    file_location: str,
+    file_format: str = "csv",
+    header=True,
+    mode: str = "x",
+    sep=",",
+    fail_on_error=True,
 ):
-    if file_format == "json":
-        df.to_json(file_location, mode=mode)
-    elif file_format == "csv":
-        df.to_csv(file_location, mode=mode, header=header, index=False, sep=sep)
-    else:
-        raise Exception(f"File format {file_format} is not recognised")
+    try:
+        if file_format == "json":
+            df.to_json(file_location, mode=mode)
+        elif file_format == "csv":
+            df.to_csv(file_location, mode=mode, header=header, index=False, sep=sep)
+        else:
+            raise Exception(f"File format {file_format} is not recognised")
+    except Exception as e:
+        print(e)
+        if fail_on_error is True:
+            raise e
 
 
 def extract_day_from_dataframe(df: pd.DataFrame, day: str, col: str) -> pd.DataFrame:
